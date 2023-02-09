a3bc817876d129c6da2ebc5248cb39c2a5bfb480
Filippos Panteli <filippos.panteli@stoics-it.com>
2023-02-09 15:21:55 +0200

    adding test heroku app

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+	res.render("home");
+})
+
+app.get("/about", function(req, res){
+	res.render("about");
+})
+
+app.listen(process.env.PORT || 3000, function(){
+	console.log("This is our simple app")
+})
+
